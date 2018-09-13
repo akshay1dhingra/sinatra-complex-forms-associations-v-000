f37ee706dba3f35c1d22ad1175b24a029e418172
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-09-13 21:38:43 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -30,8 +30,9 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
+    binding.pry
     @owner = Owner.find(params[:id])
-    
+
     if !params[:owner].keys.include?("pet_ids")
     params[:owner]["pet_ids"] = []
     end
